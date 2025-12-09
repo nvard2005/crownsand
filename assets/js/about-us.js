54b62dd27730a3b6e9cc0188ef20678178a64778
nvard2005 <nvard.m.y@gmail.com>
2025-12-09 11:03:43 +0400

    б

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -1,8 +1,10 @@
-  var swiper = new Swiper(".myGallery", {
-    loop: true,
-    speed: 600,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+        speed: 600,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
